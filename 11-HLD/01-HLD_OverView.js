d40c1f7b69f7f3c86e8693157a8f286f931ec267
Ayush Raj <rajrakshal20@gmail.com>
2024-07-29 14:38:53 +0530

    OverView

---
+++ 11-HLD/01-HLD_OverView.js
@@ -0,0 +1,18 @@
+/*
+What is HLD, Explain?
+
+High-Level Design (HLD) is a phase in the software development life cycle (SDLC) where the overall architecture and structure of
+a software system or application are planned and documented comprehensively.
+
+It is an essential step that bridges the gap between the requirements specification and the actual implementation of the system.
+The goal of HLD is to provide a blueprint or roadmap for how the system will function and how its various components will interact.
+
+
+
+
+
+
+
+
+
+*/
